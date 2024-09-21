e23b2e5e3a97822c06484537527db84cc65e5cbf
Asimthande <thanda@zulzi.com>
2024-09-21 22:30:05 +0200

    dasdas
    asd

--- drizzle/0001_happy_spacker_dave.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "post" DROP CONSTRAINT "post_group_id_user_id_fk";
---> statement-breakpoint
-ALTER TABLE "post" DROP COLUMN IF EXISTS "group_id";
--- drizzle/0004_colorful_swarm.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE "session";--> statement-breakpoint
-DROP TABLE "user";--> statement-breakpoint
-DROP TABLE "post";
